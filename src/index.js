69ce98b4bb7a0515d2b57e2dd6e64684d8fbcb62
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-13 22:01:48 +0300

    add layout

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 
 app.all('/', (req, res) =>{
-    res.render('index', {layout: false});
+    res.render('index');
 });
 
 app.listen(5001, console.log.bind(console, 'App s running on http://localhost:5001'));
