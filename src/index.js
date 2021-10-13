b00002445b264c2569fd9f3717b22280f75ef992
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-13 21:54:00 +0300

    add view engine

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+
+app.set('views', path.resolve('./src/views'))
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
 
 app.all('/', (req, res) =>{
-    res.write('Its working');
-    res.end();
+    res.render('index', {layout: false});
 });
 
 app.listen(5001, console.log.bind(console, 'App s running on http://localhost:5001'));
