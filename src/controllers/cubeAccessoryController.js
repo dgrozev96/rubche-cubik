3fc28def05ad8b4dd8fda2a48b83545e2fc55f2b
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-18 10:57:00 +0300

    show accessory select

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,9 +1,11 @@
 const router = require('express').Router({ mergeParams: true});
+const accessoryService = require('../services/accessoryService');
 const cubeService = require('../services/cubeService');
 
 router.get('/add', async (req,res) =>{
     let cube = await cubeService.getOne(req.params.cubeId)
-    res.render('cube/accessory/add' , { ...cube})
+    let accessories = await accessoryService.getAll();
+    res.render('cube/accessory/add' , { ...cube, accessories})
     
 })
 module.exports = router;
