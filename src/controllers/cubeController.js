8183ba91d0bfa9d300408f2ecc9097a0aa4f713c
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-18 12:45:59 +0300

    move create template to cube folder

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 const getCreateCubePage = (req, res) => {
     
-    res.render('create');
+    res.render('cube/create');
 };
 
 const  createCube = async (req, res) => {
