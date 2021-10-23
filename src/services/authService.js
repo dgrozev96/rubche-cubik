3fe34b7039b5aa1587545241bc30405ff8c9a188
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-23 16:57:18 +0300

    verify password on login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,28 @@
-// const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 
 
-exports.register = function(username,password, repeatPassword) {
+exports.register = function (username, password, repeatPassword) {
+    //validate password
     // return bcrypt.hash(password, 10)
     // .then(hash => User.create({username, password: hash}))
 
-    return User.create({username, password})
+    return User.create({ username, password })
+}
+
+exports.login = function (username, password) {
+    return User.findByUsername(username)
+        .then(user => {
+            return Promise.all([bcrypt.compare(password, user.password), user])
+
+        })
+        .then(([isValid, user]) => {
+            console.log(values)
+            if(isValid){
+                return user;
+            } else {    
+                throw {message: 'Username or password are invalid'}
+            }
+        })
 }
