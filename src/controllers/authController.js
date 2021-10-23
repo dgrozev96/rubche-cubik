f61f4082253dcc1e04186874704004edbaaca6c9
Damian Grozev <38095687+dgrozev96@users.noreply.github.com>
2021-10-23 10:53:44 +0300

    add user model

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const authService = require('./services/authService');
 
 router.get('/login', (req, res) => {
     res.render('auth/login')   
@@ -8,5 +9,16 @@ router.post('/login', (req, res) => {
     console.log(req.body);
     res.redirect('/login')
 })
+
+router.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
+router.post('/register', (req, res) => {
+    let {username, password, repeatPassword} = req.body;
+
+    authService.register(username, password, repeatPassword)
+}
+)
 
 module.exports = router;
